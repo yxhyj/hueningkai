57103e19b2e8ed0a110dd35d718ae734ff378913
yxhyj <a0961110268@gmail.com>
2026-04-30 16:21:13 +0800

    1

---
+++ script.js
@@ -0,0 +1,13 @@
+// Smooth scroll for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function(e) {
+    e.preventDefault();
+
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({
+        behavior: 'smooth'
+      });
+    }
+  });
+});
